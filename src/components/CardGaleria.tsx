edc38a37618064d648e3c055d534e1759a1f7535
Carlos Rodriguez <32100019+Cvisual@users.noreply.github.com>
2023-10-31 11:53:56 -0500

    fix
    se actualiza el puerto de conexion

--- src/components/CardGaleria.tsx
+++ src/components/CardGaleria.tsx
@@ -4,7 +4,7 @@ const CardGaleria = (props: Galerias) => {
   const galeria = props.item
   return (
     <div className="shadow-xl p-4 rounded-xl">
-      <img src={import.meta.env.VITE_URL + `/${galeria.imagen}`} />
+      <img src={`${galeria.imagen}`} />
       <p className="mb-4 break-all">
         <span className="font-bold uppercase">Descripción:</span>
         <br />
